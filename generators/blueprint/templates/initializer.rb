48a22f3231d0375fb26710f3bba476ae10dcaf5c
Chris Eppstein <chris@eppsteins.net>
2008-07-07 12:58:33 -0700

    changes to make the generator work right

--- generators/blueprint/templates/initializer.rb
+++ generators/blueprint/templates/initializer.rb
@@ -9,5 +9,5 @@ template_locations << "#{RAILS_ROOT}/vendor/plugins/blueprint-sass/blueprint"
 css_locations << "#{RAILS_ROOT}/public/stylesheets"
 css_locations << "#{RAILS_ROOT}/public/stylesheets/blueprint"
 
-Sass::Plugin.options[:template_location] = template_locations
-Sass::Plugin.options[:css_location] = css_locations
+Sass::Plugin.options[:template_location] = template_locations.join(':')
+Sass::Plugin.options[:css_location] = css_locations.join(':')
