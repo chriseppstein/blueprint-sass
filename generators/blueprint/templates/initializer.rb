a581d036828a1592e6b59a777bb4defa63ce6004
Joshua Bates <joshuabates@gmail.com>
2008-07-21 11:25:38 -0700

    Update the initializer generator template to work with the new template_location hash

--- generators/blueprint/templates/initializer.rb
+++ generators/blueprint/templates/initializer.rb
@@ -1,11 +1,5 @@
-template_locations = Sass::Plugin.options[:template_location].split(':')
-css_locations      = Sass::Plugin.options[:css_location].split(':')
+Sass::Plugin.options[:template_location] = {
+  "#{RAILS_ROOT}/app/stylesheets" => "#{RAILS_ROOT}/public/stylesheets",
+  "#{RAILS_ROOT}/vendor/plugins/blueprint-sass/blueprint" => "#{RAILS_ROOT}/public/stylesheets/blueprint"
+}
 
-template_locations << "#{RAILS_ROOT}/app/stylesheets"
-template_locations << "#{RAILS_ROOT}/vendor/plugins/blueprint-sass/blueprint"
-
-css_locations << "#{RAILS_ROOT}/public/stylesheets"
-css_locations << "#{RAILS_ROOT}/public/stylesheets/blueprint"
-
-Sass::Plugin.options[:template_location] = template_locations.join(':')
-Sass::Plugin.options[:css_location] = css_locations.join(':')
